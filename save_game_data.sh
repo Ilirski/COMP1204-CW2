eb03e98b5ecc40c80007597da8f88a055cd41a5a
Illyria <24876805+Ilirski@users.noreply.github.com>
2023-04-21 16:50:39 +0800

    Add MySQL queries for many to many insertion

--- save_game_data.sh
+++ save_game_data.sh
@@ -4,14 +4,23 @@
 [ $# -ge 1 ] && [ -f "$1" ] && INPUT="$1" || INPUT="-"
 INPUT=$(cat "$INPUT")
 
-insert_value() {
+insert_value_many_to_many() {
+    # Insert value into join table of many-to-many relationship (https://stackoverflow.com/a/19734088/17771525)
     local table_name="$1"
     local column_name="$2"
     local value="$3"
 
     if [ -n "$value" ]; then
-        local sql="INSERT INTO $table_name ($column_name) VALUES ('$value');"
-        echo "$sql"
+        # Insert into `tag` table, ignoring duplicate keys (https://stackoverflow.com/a/1361368/17771525)
+        local query_sql="INSERT INTO $table_name ($column_name) VALUES ('$value')"
+        query_sql+=" ON DUPLICATE KEY UPDATE ${table_name}_id=${table_name}_id;"
+        query_sql+=" SET @${table_name}_id = LAST_INSERT_ID();"
+        echo "$query_sql"
+        # mysql -u root steam_games_db -e "$sql"
+        
+        # Insert into `app_${table_name}` table
+        local app_table_sql="INSERT INTO app_${table_name} (app_id, ${table_name}_id) VALUES (@app_id, @${table_name}_id);"
+        echo "$app_table_sql"
         # mysql -u root steam_games_db -e "$sql"
     fi
 }
@@ -35,23 +44,30 @@ readarray -t a <<<"$INPUT"
 
 # Insert into `app` table
 # insert into tbl (col1,col2) values (nullif('$col1','specialnullvalue') (https://stackoverflow.com/a/75712852/17771525)
-app_sql="INSERT INTO app (app_id, app_name, app_type, app_storename, app_change_number, app_last_change_date, app_release_date)"
-app_sql+=" VALUES (${a[0]}, '${a[1]}', '${a[2]}', '${a[3]}', ${a[8]}, '${a[9]}', '${a[10]})';"
+app_sql="INSERT INTO app (app_id, app_name, app_type, app_store_name, app_change_number, app_last_change_date, app_release_date)"
+app_sql+=" VALUES (${a[0]}, '${a[1]}', '${a[2]}', '${a[3]}', '${a[8]}', '${a[9]}', '${a[10]}';)"
+app_sql+=" SET @app_id = LAST_INSERT_ID();"
 # mysql -u root -e steam_games_db "$app_sql"
 echo "$app_sql"
 
 # Check if developer, publisher, and franchise is not empty. If not empty, insert into their respective table
-insert_value "developer" "developer_name" "${a[4]}"
-insert_value "publisher" "publisher_name" "${a[5]}"
-insert_value "franchise" "franchise_name" "${a[6]}"
+insert_value_many_to_many "developer" "developer_name" "${a[4]}"
+insert_value_many_to_many "publisher" "publisher_name" "${a[5]}"
+insert_value_many_to_many "franchise" "franchise_name" "${a[6]}"
 
 # Split the data into individual tags separated by comma
 tags=$(echo "${a[11]}" | tr ',' '\n')
 
 # Iterate over tags
 while read -r tag; do
-    # Insert into `tag` table
-    tag_sql="INSERT INTO tag (tag_name) VALUES ('$tag');"
+    tag_sql="INSERT INTO tag (tag_name) VALUES ('$tag')"
+    tag_sql+=" ON DUPLICATE KEY UPDATE tag_id=tag_id;"
+    tag_sql+=" SET @tag_id = LAST_INSERT_ID();"
     echo "$tag_sql"
     # mysql -u root -e steam_games_db "$tag_sql"
+    
+    # Insert into `app_tag` join table
+    app_tag_sql="INSERT INTO app_tag (app_id, tag_id) VALUES (@app_id, @tag_id);"
+    echo "$app_tag_sql"
+    # mysql -u root -e steam_games_db "$app_tag_sql"
 done <<< "$tags"
--- steam_games_db_create.sql
+++ steam_games_db_create.sql
@@ -1,20 +1,52 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2023-04-19 10:18:49.355
+-- Last modification date: 2023-04-21 08:41:28.501
 
 -- tables
 -- Table: app
 CREATE TABLE app (
     app_id int  NOT NULL,
     app_name varchar(200)  NOT NULL,
-    publisher_id int  NOT NULL,
+    app_type varchar(20)  NOT NULL,
+    app_store_name varchar(50)  NOT NULL,
+    app_change_number char(8)  NOT NULL,
+    app_last_change_date timestamp  NOT NULL,
+    app_release_date timestamp  NOT NULL,
     CONSTRAINT app_pk PRIMARY KEY (app_id)
 );
 
--- Table: app_operating_system
-CREATE TABLE app_operating_system (
+-- Table: app_developer
+CREATE TABLE app_developer (
+    app_id int  NOT NULL,
+    developer_id int  NOT NULL,
+    CONSTRAINT developer_app_pk PRIMARY KEY (developer_id,app_id)
+);
+
+-- Table: app_franchise
+CREATE TABLE app_franchise (
+    app_id int  NOT NULL,
+    franchise_id int  NOT NULL,
+    CONSTRAINT developer_app_pk PRIMARY KEY (franchise_id,app_id)
+);
+
+-- Table: app_os
+CREATE TABLE app_os (
     app_id int  NOT NULL,
     os_id int  NOT NULL,
-    CONSTRAINT app_operating_system_pk PRIMARY KEY (app_id,os_id)
+    CONSTRAINT app_operating_system_pk PRIMARY KEY (os_id,app_id)
+);
+
+-- Table: app_publisher
+CREATE TABLE app_publisher (
+    app_id int  NOT NULL,
+    publisher_id int  NOT NULL,
+    CONSTRAINT developer_app_pk PRIMARY KEY (publisher_id,app_id)
+);
+
+-- Table: app_tag
+CREATE TABLE app_tag (
+    app_id int  NOT NULL,
+    tag_id int  NOT NULL,
+    CONSTRAINT tag_app_pk PRIMARY KEY (tag_id,app_id)
 );
 
 -- Table: developer
@@ -24,11 +56,11 @@ CREATE TABLE developer (
     CONSTRAINT developer_pk PRIMARY KEY (developer_id)
 );
 
--- Table: developer_app
-CREATE TABLE developer_app (
-    developer_id int  NOT NULL,
-    app_id int  NOT NULL,
-    CONSTRAINT developer_app_pk PRIMARY KEY (developer_id,app_id)
+-- Table: franchise
+CREATE TABLE franchise (
+    franchise_id int  NOT NULL,
+    franchise_name varchar(100)  NOT NULL,
+    CONSTRAINT developer_pk PRIMARY KEY (franchise_id)
 );
 
 -- Table: log
@@ -54,8 +86,8 @@ CREATE TABLE log (
     CONSTRAINT log_pk PRIMARY KEY (log_id)
 );
 
--- Table: operating_system
-CREATE TABLE operating_system (
+-- Table: os
+CREATE TABLE os (
     os_id int  NOT NULL,
     os_name varchar(20)  NOT NULL,
     CONSTRAINT operating_system_pk PRIMARY KEY (os_id)
@@ -75,44 +107,49 @@ CREATE TABLE tag (
     CONSTRAINT tag_pk PRIMARY KEY (tag_id)
 );
 
--- Table: tag_app
-CREATE TABLE tag_app (
-    tag_id int  NOT NULL,
-    app_id int  NOT NULL,
-    CONSTRAINT tag_app_pk PRIMARY KEY (tag_id,app_id)
-);
-
 -- foreign keys
--- Reference: app_operating_system_app (table: app_operating_system)
-ALTER TABLE app_operating_system ADD CONSTRAINT app_operating_system_app FOREIGN KEY app_operating_system_app (app_id)
+-- Reference: app_operating_system_app (table: app_os)
+ALTER TABLE app_os ADD CONSTRAINT app_operating_system_app FOREIGN KEY app_operating_system_app (os_id)
     REFERENCES app (app_id);
 
--- Reference: app_operating_system_operating_system (table: app_operating_system)
-ALTER TABLE app_operating_system ADD CONSTRAINT app_operating_system_operating_system FOREIGN KEY app_operating_system_operating_system (os_id)
-    REFERENCES operating_system (os_id);
+-- Reference: app_operating_system_operating_system (table: app_os)
+ALTER TABLE app_os ADD CONSTRAINT app_operating_system_operating_system FOREIGN KEY app_operating_system_operating_system (app_id)
+    REFERENCES os (os_id);
 
--- Reference: app_publisher (table: app)
-ALTER TABLE app ADD CONSTRAINT app_publisher FOREIGN KEY app_publisher (publisher_id)
-    REFERENCES publisher (publisher_id);
-
--- Reference: developer_app_app (table: developer_app)
-ALTER TABLE developer_app ADD CONSTRAINT developer_app_app FOREIGN KEY developer_app_app (app_id)
+-- Reference: developer_app_app (table: app_developer)
+ALTER TABLE app_developer ADD CONSTRAINT developer_app_app FOREIGN KEY developer_app_app (app_id)
     REFERENCES app (app_id);
 
--- Reference: developer_app_developer (table: developer_app)
-ALTER TABLE developer_app ADD CONSTRAINT developer_app_developer FOREIGN KEY developer_app_developer (developer_id)
+-- Reference: developer_app_developer (table: app_developer)
+ALTER TABLE app_developer ADD CONSTRAINT developer_app_developer FOREIGN KEY developer_app_developer (developer_id)
     REFERENCES developer (developer_id);
+
+-- Reference: franchise_app_app (table: app_franchise)
+ALTER TABLE app_franchise ADD CONSTRAINT franchise_app_app FOREIGN KEY franchise_app_app (app_id)
+    REFERENCES app (app_id);
+
+-- Reference: franchise_app_franchise (table: app_franchise)
+ALTER TABLE app_franchise ADD CONSTRAINT franchise_app_franchise FOREIGN KEY franchise_app_franchise (franchise_id)
+    REFERENCES franchise (franchise_id);
 
 -- Reference: log_game (table: log)
 ALTER TABLE log ADD CONSTRAINT log_game FOREIGN KEY log_game (app_id)
     REFERENCES app (app_id);
 
--- Reference: tag_app_app (table: tag_app)
-ALTER TABLE tag_app ADD CONSTRAINT tag_app_app FOREIGN KEY tag_app_app (app_id)
+-- Reference: publisher_app_app (table: app_publisher)
+ALTER TABLE app_publisher ADD CONSTRAINT publisher_app_app FOREIGN KEY publisher_app_app (app_id)
     REFERENCES app (app_id);
 
--- Reference: tag_app_tag (table: tag_app)
-ALTER TABLE tag_app ADD CONSTRAINT tag_app_tag FOREIGN KEY tag_app_tag (tag_id)
+-- Reference: publisher_app_publisher (table: app_publisher)
+ALTER TABLE app_publisher ADD CONSTRAINT publisher_app_publisher FOREIGN KEY publisher_app_publisher (publisher_id)
+    REFERENCES publisher (publisher_id);
+
+-- Reference: tag_app_app (table: app_tag)
+ALTER TABLE app_tag ADD CONSTRAINT tag_app_app FOREIGN KEY tag_app_app (app_id)
+    REFERENCES app (app_id);
+
+-- Reference: tag_app_tag (table: app_tag)
+ALTER TABLE app_tag ADD CONSTRAINT tag_app_tag FOREIGN KEY tag_app_tag (tag_id)
     REFERENCES tag (tag_id);
 
 -- End of file.
