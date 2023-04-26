02ea002304bf9df5f7f7a72c80440a385a26146c
Illyria <24876805+Ilirski@users.noreply.github.com>
2023-04-26 23:35:26 +0800

    Script now informs the user if database exists or schema failed to load

--- create_steam_games_db.sh
+++ create_steam_games_db.sh
@@ -10,8 +10,14 @@ echo -ne "Creating steam_games_db database...\r"
 
 if mysql -u root -e "CREATE DATABASE steam_games_db;"; then
     echo -ne "Populating steam_games_db with tables...\r"
-    if mysql -u root steam_games_db <steam_games_db_create.sql; then
-        echo -ne "steam_games_db successfully created!    \r"
-        echo -ne '\n'
-    fi
+else
+    echo -ne "steam_games_db already exists!    \r"
+    exit 1
+fi
+
+if mysql -u root steam_games_db <steam_games_db_create.sql; then
+    echo -ne "steam_games_db successfully created!    \r"
+else
+    echo -ne "steam_games_db could not be created!    \r"
+    exit 1
 fi
