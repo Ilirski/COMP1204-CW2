5acf6c1003a66479d64721fc3c7a1cb8eb6ca9d8
Illyria <24876805+Ilirski@users.noreply.github.com>
2023-04-30 17:51:35 +0800

    Added devs and os plots

--- plot_steam_games_data.sh
+++ plot_steam_games_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 usage() {
-    cat <<HELP_USAGE
+    cat <<EOM
 
     $0 [OPTIONS] ...
 
@@ -10,15 +10,12 @@ usage() {
             Show this help message and exit.
         -t, --tags
             Plot most popular tags.
+        -d, --devs
+            Plot games per developers.
+        -o, --os
+            Plot most popular OS.
 
-HELP_USAGE
-    exit 1
-}
-
-die() {
-    # complain to STDERR and exit with error
-    echo "$*" >&2
-    exit 2
+EOM
 }
 
 plot_most_popular_tags() {
@@ -52,6 +49,62 @@ EOM
 EOM
 }
 
+plot_games_per_developers() {
+    read -r -d '' sql_query <<-EOM
+    SELECT developer.developer_name, COUNT(DISTINCT app_developer.app_id) as num_games
+    FROM developer
+    JOIN app_developer ON developer.developer_id = app_developer.developer_id
+    GROUP BY developer.developer_id
+    ORDER BY num_games DESC;
+EOM
+
+    mysql steam_games_db -u root -B <<<"$sql_query" >"$tmp_file"
+
+    gnuplot <<-EOM
+    set terminal pngcairo size 1920,1080 enhanced font 'Verdana,10'
+    set output 'games_per_developers.png'
+    set title "Games per developers"
+    set xlabel "Developers"
+    set ylabel "Number of games"
+    set xrange [0:15]
+    set style data histogram
+    set style histogram cluster gap 1
+    set style fill solid border -1
+    set xtics rotate by -45
+    set grid y
+    set datafile separator "\t"
+    plot "$tmp_file" using 2:xtic(1) title "Games per developer"
+EOM
+}
+
+plot_most_popular_os() {
+    read -r -d '' sql_query <<-EOM
+    SELECT os.os_name, COUNT(DISTINCT app_os.app_id) as num_games
+    FROM os
+    JOIN app_os ON os.os_id = app_os.os_id
+    GROUP BY os.os_id
+    ORDER BY num_games DESC;
+EOM
+
+    mysql steam_games_db -u root -B <<<"$sql_query" >"$tmp_file"
+
+    gnuplot <<-EOM
+    set terminal pngcairo size 1920,1080 enhanced font 'Verdana,10'
+    set output 'most_popular_os.png'
+    set title "Most popular OS"
+    set xlabel "OS"
+    set ylabel "Number of games"
+    set xrange [0:15]
+    set style data histogram
+    set style histogram cluster gap 1
+    set style fill solid border -1
+    set xtics rotate by -45
+    set grid y
+    set datafile separator "\t"
+    plot "$tmp_file" using 2:xtic(1) title "Games per OS"
+EOM
+}
+
 # If no arguments are supplied, display usage (https://stackoverflow.com/a/687816/17771525)
 [ -z "$1" ] && { usage; }
 
@@ -71,26 +124,34 @@ if [ ! -f "$tmp_file" ]; then
     exit 1
 fi
 
-# Getopts (https://stackoverflow.com/a/28466267/17771525)
-while getopts "ht:-:" OPT; do
-    if [ "$OPT" = "-" ]; then     # long option: reformulate OPT and OPTARG
-        OPT="${OPTARG%%=*}"       # extract long option name
-        OPTARG="${OPTARG#"$OPT"}" # extract long option argument (may be empty)
-        OPTARG="${OPTARG#=}"      # if long option argument, remove assigning `=`
-    fi
-    case $OPT in
-    h | help) # display usage
+TEMP=$(getopt -o htdo --long help,tags,devs,os -n "$0" -- "$@")
+
+eval set -- "$TEMP"
+
+# extract options (https://stackoverflow.com/a/28466267/17771525)
+while true; do
+    case "$1" in
+    -h | --help)
         usage
+        exit 1
         ;;
-    t | tags) # plot most popular tags
+    -t | --tags)
         plot_most_popular_tags
+        exit 0
         ;;
-    ??*)
-        die "illegal option --$OPT"
-        ;; # bad long option
-    ?)
-        exit 2
-        ;; # bad short option (error reported via getopts)
+    -d | --devs)
+        plot_games_per_developers
+        exit 0
+        ;;
+    -o | --os)
+        plot_most_popular_os
+        exit 0
+        ;;
+    --)
+        exit 1
+        ;;
+    *)
+        exit 1
+        ;;
     esac
 done
-shift $((OPTIND - 1)) # remove parsed options and args from $@ list
